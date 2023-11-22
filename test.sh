2f32be808283a6d6838b56034785a1cdd37fe2a2
angelpilled <angelpilleddev@gmail.com>
2023-11-22 22:29:02 +0100

    EOF user section fixed

--- test.sh
+++ test.sh
@@ -1,3 +1,17 @@
-read -p "Please enter your language: " langSelect 
-langSelect=$(echo ${langSelect,,})
-echo ${langSelect}
+cat <<'ENDOFSCRIPT' > ./userinstall.sh
+    echo "User was added"
+    read -p "Please enter keyboard language: " keyLang 
+    keyLang=$(echo ${keyLang,,})
+    echo ${keyLang}
+
+    echo "Setting up language and locale..."
+
+    case $keyLang in
+	hungarian)
+	    echo "Hungarian layout set up."
+	    ;;
+	*)
+	    echo ""
+	    ;;
+    esac
+ENDOFSCRIPT
